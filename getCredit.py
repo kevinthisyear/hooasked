285b7a1dcc097ed67a0bf67dadffb7a3cf558962
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2023-03-25 19:49:21 -0400

    python file

---
+++ getCredit.py
@@ -0,0 +1,70 @@
+
+
+
+# initialize user inputs
+x = int(input("What is the length of your credit history (in years): "))
+y = int(input("How many accounts do you have? "))
+z = int(input("How many hard inquiries have you had within the last 2 years? "))
+a = int(input("What is your credit limit? "))
+b = int(input("What is your amount of credit owed? "))
+c = int(input("How many payments have you missed in the last 24 months? "))
+
+
+def Calculate(lengthOfCreditHistory, totalAccounts, Inquiries, creditLimit, owed, History):
+
+    # initialize users credit score
+    score = 0
+
+    # length of credit history affects 15% of your credit score ~127.5 points
+    if lengthOfCreditHistory <= 3:
+        score = score + 20
+    if 3 < lengthOfCreditHistory < 15:
+        score = score + 89
+    if lengthOfCreditHistory >= 15:
+        score = score + 127
+
+    # total accounts affects 10% of your credit score ~85 points
+    if totalAccounts <= 5:
+        score += 85
+    if 5 < totalAccounts < 10:
+        score = score + 60
+    if totalAccounts >= 10:
+        score = score + 0
+
+    # Inquiries affects 10% of your credit score ~85 points
+    if Inquiries == 0:
+        score += 85
+    if Inquiries == 1:
+        score = score + 60
+    if Inquiries > 1:
+        score = score + 0
+
+    # Amount Owed affects 30% of your credit score ~255 points
+    if owed == 0:
+        score += 255
+
+    # Payment History affects 35% of your credit score ~297 points
+    if History == 0:
+        score += 297
+
+    return score
+
+total = Calculate(x,y,z,a,b,c)
+
+if total == 0:
+    print(str(total) + ": How Does this happen; your score is impossibly low")
+
+if total < 580:
+    print(str(total) + ": Poor | Your score is well below the average score of U.S. consumers and demonstrates to lenders that you are a risky borrower.")
+
+if 580 < total < 669:
+    print(str(total) + ": Fair | Your score is below the average score of U.S. consumers, though many lenders will approve loans with this score.")
+
+if 670 < total < 739:
+    print(str(total) + ": Good | Your score is near or slightly above the average of U.S. consumers and most lenders consider this a good score.")
+
+if 740 < total < 799:
+    print(str(total) + ": Very Good | Your score is above the average of U.S. consumers and demonstrates to lenders that you are a very dependable borrower.")
+
+if total > 800:
+    print(str(total) + ": Exceptional | Your score is well above the average score of U.S. consumers and clearly demonstrates to lenders that you are an exceptional borrower.")
